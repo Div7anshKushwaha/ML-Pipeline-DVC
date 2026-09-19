0860b7d090259add204cbb2f105914be44c49aa5
Divyansh Kushwaha <divyanshh.ds@gmail.com>
2026-09-19 22:45:02 +0530

    adding cookiecutter project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An end-to-end MLOps project demonstrating reproducible machine learning workflows using DVC for data versioning and pipeline management, with automated data ingestion, preprocessing, feature engineering, model training, and evaluation.',
+    author='Divyansh Kushwaha',
+    license='MIT',
+)
